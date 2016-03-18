d5e27a2d6b6d99d7709313942164d63d2500ac93
Thomas Di Meco <git@tdimeco.fr>
2016-03-19 00:31:45 +0100

    Support for adding and playing sounds !

--- QuickSound/PopoverViewController.swift
+++ QuickSound/PopoverViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import Cocoa
+import CoreAudio
 
 
 class PopoverViewController: NSViewController {
     
-    // MARK: - Lifecycle
+    @IBOutlet var arrayController: NSArrayController!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do view setup here.
+    
+    // MARK: - UI actions
+    
+    @IBAction func tableViewRowDoubleClicked(tableView: NSTableView) {
+        guard tableView.clickedRow >= 0 else { return }
+        guard let soundDic = self.arrayController.arrangedObjects[tableView.clickedRow] as? NSDictionary else { return }
+        guard let soundObj = Sound(dictionary: soundDic) else { return }
+        
+        // Play sound
+        if let soundURL = NSURL(string: soundObj.filePath) {
+            let sound = NSSound(contentsOfURL: soundURL, byReference: true)
+            let successOrNil = sound?.play()
+            
+            // Show alert
+            if successOrNil == nil || !successOrNil! {
+                let alert = NSAlert()
+                alert.messageText = "Impossible de lire le son"
+                alert.informativeText = "Le fichier est invalide ou introuvable."
+                alert.alertStyle = .WarningAlertStyle
+                alert.runModal()
+            }
+        }
     }
     
-    
-    // MARK: - Menu actions
+    @IBAction func addSoundAction(sender: AnyObject) {
+        
+        // Configure open panel
+        let openPanel = NSOpenPanel()
+        openPanel.canChooseFiles = true
+        openPanel.canChooseDirectories = false
+        openPanel.canCreateDirectories = true
+        openPanel.allowsMultipleSelection = true
+        
+        // Open panel
+        let result = openPanel.runModal()
+        if result == NSFileHandlingPanelOKButton {
+            
+            // Save each selected sound
+            for fileURL in openPanel.URLs {
+                let filePath = fileURL.absoluteString
+                let soundDic = Sound(filePath: filePath).toDictionary()
+                self.arrayController.addObject(soundDic)
+            }
+        }
+    }
     
     @IBAction func showAboutWindow(sender: AnyObject) {
         // TODO
---
+++ QuickSound/TransformerFilePathToName.swift
@@ -0,0 +1,25 @@
+//
+//  TransformerFilePathToName.swift
+//  QuickSound
+//
+//  Created by Thomas Di Meco on 18/03/2016.
+//  Copyright © 2016 Thomas Di Meco. All rights reserved.
+//
+
+import Foundation
+
+
+@objc(TransformerFilePathToName)
+class TransformerFilePathToName: NSValueTransformer {
+    
+    // MARK: - Value transformer
+    
+    override class func transformedValueClass() -> AnyClass {
+        return NSString.self
+    }
+    
+    override func transformedValue(value: AnyObject?) -> AnyObject? {
+        guard let filePath = value as? String else { return nil }
+        return NSURL(string: filePath)?.lastPathComponent
+    }
+}
