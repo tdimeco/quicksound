f4fbaf375dc98ff80ee9ed485051a16e81acd99d
Thomas Di Meco <git@tdimeco.fr>
2016-03-23 00:13:27 +0100

    Check for updates directly from the master branch on GitHub

---
+++ QuickSound/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  QuickSound
+//
+//  Created by Thomas Di Meco on 22/03/2016.
+//  Copyright © 2016 Thomas Di Meco. All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    static let UpdatePlistURL = "https://raw.githubusercontent.com/tdimeco/QuickSound/master/QuickSound/Info.plist"
+    static let UpdatesPageURL = "https://github.com/tdimeco/QuickSound/releases"
+}
